d4453ec1bd893352fcee2144805f4079f47c82c5
zhyang <yangzh0001@sina.cn>
2021-07-06 09:01:14 +0800

    init

---
+++ video_play/src/main/java/com/video/video_play/VideoPlayApplication.java
@@ -0,0 +1,13 @@
+package com.video.video_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPlayApplication.class, args);
+    }
+
+}
